3266022b546cb97039cfad4f7746a19457573c88
Samoylov Dmitriy <78538544+BeardBoy1987@users.noreply.github.com>
2021-10-21 12:24:07 +0400

    homework 1 adedd, skills block modified

--- src/pages/main/script.js
+++ src/pages/main/script.js
@@ -1,15 +1,73 @@
-let userName = prompt("Как вас зовут?");
-let userSurname = prompt("Ваша фамилия?");
-let userAge = prompt("Сколько вам лет?");
+// let userName = prompt("Как вас зовут?");
+// let userSurname = prompt("Ваша фамилия?");
+// let userAge = prompt("Сколько вам лет?");
 
-alert(userName);
-alert(userSurname);
-alert(userAge);
+// alert(userName);
+// alert(userSurname);
+// alert(userAge);
 
-let userPersonalInfo = {
-  name: userName,
-  surname: userSurname,
-  age: userAge
-};
+// let userPersonalInfo = {
+//   name: userName,
+//   surname: userSurname,
+//   age: userAge
+// };
 
-console.log(userPersonalInfo);
+// console.log(userPersonalInfo);
+
+// FIRST TASK
+
+// let i = prompt("Введите число");
+// let n = 0;
+
+// while (n++ < i) {
+//   if (Number.isInteger(n / 4)) continue;
+// else {
+//     console.log(n)
+//   }
+// }
+
+// SECOND TASK
+
+// let i = prompt("Введите число");
+// let n = 1;
+
+// while(i) n *= (i--);
+// console.log(n);
+
+// THIRD TASK
+
+// let i = prompt("Введите число");
+// let n = prompt("Введите степень");
+// let result = (Math.pow(i, n));
+
+// i, n = Number(i, n);
+
+// for (i, n; ; result) {
+//   console.log(result)
+//   break;
+// }
+
+// TEST
+
+// if (isNaN(i, n)) {
+//   alert("Недопустимые символы")
+// }
+// if (i,n == "") {
+//   alert("empty")
+// }
+// if (i,n < 0) {
+//   alert("значение не может быть отрицательным")
+// }
+
+// FOURTH TASK
+
+// let rand = Math.floor(1 + Math.random() * 10);
+// console.log(rand);
+
+// while (true) {
+//   let i = +prompt("Введите число");
+//   if  (i === rand) {
+//   alert("Угадал!")
+//   break;
+// }
+// }
